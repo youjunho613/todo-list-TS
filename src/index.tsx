3b45550fa599a3e03f55cbcd2428762fd45a61d2
youjunho613 <njn613@gmail.com>
2023-07-29 03:35:15 +0900

    feat. todoList state RTK 전역상태관리

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { App } from "App";
+import { Provider } from "react-redux";
+import store from "redux/config/configStore";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
